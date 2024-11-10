7172a1ddf1a2108bee9eb5e16ee58479ce043e01
gisele-creat <giselesj17@gmail.com>
2024-11-09 22:14:32 -0300

    user creator changes and corrections

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,72 +1,73 @@
-const userModel = require('../models/userModel')
+const userModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
 
 const createNewUser = async (req, res) => {
-    const { nome, cpf, email, telefone, senha } = req.body;
+  const { nome, cpf, email, telefone, senha } = req.body;
 
-    const saltRounds = 10
-    const senhaHast = await bcrypt.hash(senha, saltRounds)
+  const saltRounds = 10;
+  const senhaHash = await bcrypt.hash(senha, saltRounds);
 
-    const newUser = await userModel.create({
-        full_name: nome,
-        cpf: cpf,
-        email: email,
-        phone: telefone,
-        password:senha
+  const newUser = await userModel.create({
+    full_name: nome,
+    cpf: cpf,
+    email: email,
+    phone: telefone,
+    password: senhaHash,
+  });
+
+  console.log("Usuário criado com sucesso!:", newUser.id);
+
+  res.status(201).send({
+    message: `Usuário ${newUser.full_name}, id ${newUser.id} criado com sucesso! ID: ${newUser.id}`,
+  });
+};
+
+const userList = async (req, res) => {
+  try {
+    const users = await userModel.findAll({
+      order: [["id", "ASC"]], //foi adicionado.
     });
+    res.send(users);
+  } catch (error) {
+    res.send({
+      message: "Erro ao listar usuários!",
+    });
+  }
+};
 
+const userUpdate = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    await userModel.update({ ...req.body }, { where: { id: id } });
     res.status(201).send({
-        message: `Usuário ${newUser.full_name},`
-    })
-    
-    console.log("Usuário criado com sucesso!:", newUser.id)
-    
-    res.status(201).send({
-        message: `Usuário ${newUser.full_name}, id ${ newUser.id} criado com sucesso! ID: ${newUser.id}`
-    })
-
-
-
-}
-
-const userList = async (req, res, next) => {
-    try{
-     const users = await userModel.findAll();
-     res.send(users)
-    
-    }catch (error){
-        res.send({
-            message: 'Erro ao listar usuários!'
-        })
-    
-    }
-}
-
-const userUpdate = async (req, res, next) => {
-    try {
-        const id = parseInt(req.params.id)
-        await userModel.update(
-            { ...req.body },
-            { where: {id: id}}
-        );
-        res.status(201).send({
-            message: `Usuário alterado com sucesso! ID: ${id}`
-        })
-        
-    } catch (error) {
-        res.send({
-            message: `Erro ao alterar os usuários!`
-        })
-    }
+      message: `Usuário alterado com sucesso! ID: ${id}`,
+    });
+  } catch (error) {
+    res.send({
+      message: `Erro ao alterar o usuário!`,
+    });
+  }
 };
 
 const deleteUserById = async (req, res) => {
-
-}
+  try {
+    const id = parseInt(req.params.id);
+    await userModel.destroy({ 
+        where: { id: id } 
+    });
+    res.status(201).send({ 
+        message: `Usuário com ID: ${id} deletado com sucesso!` 
+    });
+  } catch (error) {
+    res.send({ 
+        message: `Erro ao deletar o usuário!` 
+    });
+  }
+};
 
 module.exports = {
-    createNewUser,
-    userList,
-    userUpdate,
-    deleteUserById
-}
+  createNewUser,
+  userList,
+  userUpdate,
+  deleteUserById,
+};
