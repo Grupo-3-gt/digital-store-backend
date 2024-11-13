4288fec3ba0115853204971aa55fa7cbe1243a6f
THAIS SOARES DA COSTA <THAIS0906@DIGITALCOLLEGE.LOCAL>
2024-11-13 15:45:41 -0300

    create feat-address

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const app = express();
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
+const addressRoutes = require('./routes/addressRoutes')
 
-app.use(userRoutes)
+app.use(userRoutes);
+
+app.use(addressRoutes);
 
 module.exports = app ;
--- src/models/addressModel.js
+++ src/models/addressModel.js
@@ -11,36 +11,66 @@ let addressModel = connection.define("address", {
       model: userModel,
       key: "id",
     },
+    validate: {
+      isInt: { msg: "O user_id deve ser um número inteiro" },
+      notNull: { msg: "O user_id é obrigatório" }, 
+    },
   },
 
   street: {
     type: DataTypes.STRING(150),
     allowNull: false,
+    validate: {
+      notEmpty: { msg: "A rua não pode estar vazia" },
+      len: { args: [1, 150], msg: "A rua deve ter entre 1 e 150 caracteres" }, 
+    },
   },
 
   number: {
     type: DataTypes.STRING(6),
     allowNull: false,
+    validate: {
+      notEmpty: { msg: "O número não pode estar vazio" },
+      isNumeric: { msg: "O número do imóvel deve conter apenas números" }, 
+      len: { args: [1, 6], msg: "O número deve ter entre 1 e 6 caracteres" }, 
+    },
   },
 
   neighborhood: {
     type: DataTypes.STRING(150),
     allowNull: false,
+    validate: {
+      notEmpty: { msg: "O bairro não pode estar vazio" },
+      len: { args: [1, 150], msg: "O bairro deve ter entre 1 e 150 caracteres" },
+    },
   },
 
   city: {
     type: DataTypes.STRING(100),
     allowNull: false,
+    validate: {
+      notEmpty: { msg: "A cidade não pode estar vazia" },
+      len: { args: [1, 100], msg: "A cidade deve ter entre 1 e 100 caracteres" },
+    },
   },
 
   cep: {
     type: DataTypes.STRING(8),
     allowNull: false,
+    validate: {
+      notEmpty: { msg: "O CEP não pode estar vazio" },
+      isNumeric: { msg: "O CEP deve conter apenas números" },
+      len: { args: [8, 8], msg: "O CEP deve ter exatamente 8 caracteres" }
+    },
   },
 
   complement: {
     type: DataTypes.STRING(150),
     allowNull: true,
+    defaultValue: null,
+    validate: {
+      len: { args: [0, 150], msg: "O complemento deve ter no máximo 150 caracteres" },
+    },
   },
 });
 
