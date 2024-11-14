843ac08e3d31e9b714abf597d5ecb8187a55e6b7
MyrelaSFerreira <myrelaferreira01@gmail>
2024-11-14 16:26:26 -0300

    create auth route

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require ('./routes/categoryRoutes')
 const addressRoutes = require('./routes/addressRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 
-app.use(userRoutes, categoryRoutes, addressRoutes);
+app.use(userRoutes, categoryRoutes, addressRoutes, authRoutes);
 
 
 module.exports = app ;
