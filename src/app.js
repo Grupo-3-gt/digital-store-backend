f06d1cd3b5946241dfff421964e6bc756642cb04
iarley1 <iarley96@gmail.com>
2024-11-21 23:47:03 -0300

    save changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes')
-const categoryRoutes = require ('./routes/categoryRoutes')
-const addressRoutes = require('./routes/addressRoutes')
-const authRoutes = require('./routes/authRoutes')
+app.use(cors());
+
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const addressRoutes = require("./routes/addressRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 app.use(userRoutes, categoryRoutes, addressRoutes, authRoutes, productRoutes);
 
-module.exports = app ;
+module.exports = app;
